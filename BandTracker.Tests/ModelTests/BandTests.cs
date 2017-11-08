69166d489659b1bb85e44af360295104875307f3
Marcus Parmentier <marcusjparmentier@gmail.com>
2017-11-08 00:27:20 -0800

    Add GetAll and DeleteAll methods to Band.cs and passes test.

---
+++ BandTracker.Tests/ModelTests/BandTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using BandTracker.Models;
+
+namespace BandTracker.Tests
+{
+  [TestClass]
+  public class BandTests : IDisposable
+  {
+    public BandTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=band_tracker_test;";
+    }
+
+    public void Dispose()
+    {
+      Venue.DeleteAll();
+      Band.DeleteAll();
+    }
+
+    [TestMethod]
+    public void GetAll_BandsEmptyAtFirst_0()
+    {
+      //Arrange, Act
+      int result = Band.GetAll().Count;
+
+      //Assert
+      Assert.AreEqual(0, result);
+    }
+  }
+}
--- BandTracker/Models/Band.cs
+++ BandTracker/Models/Band.cs
@@ -1,20 +1,58 @@
-// using System.Collections.Generic;
-// using MySql.Data.MySqlClient;
-// using System;
-//
-// namespace BandTracker.Models
-// {
-//   public class Band
-//   {
-//     private string _bandName;
-//     private string _genre;
-//     private int _id;
-//
-//     public Band(string bandName, string genre, int id = 0)
-//     {
-//       _bandName = bandName;
-//       _genre = genre;
-//       _id = id;
-//     }
-//   }
-// }
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace BandTracker.Models
+{
+  public class Band
+  {
+    private string _bandName;
+    private string _genre;
+    private int _id;
+
+    public Band(string bandName, string genre, int id = 0)
+    {
+      _bandName = bandName;
+      _genre = genre;
+      _id = id;
+    }
+
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM bands;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int BandId = rdr.GetInt32(0);
+        string BandTitle = rdr.GetString(1);
+        string BandGenre = rdr.GetString(2);
+        Band newBand = new Band(BandTitle, BandGenre, BandId);
+        allBands.Add(newBand);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allBands;
+    }
+
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM bands;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+  }
+}
