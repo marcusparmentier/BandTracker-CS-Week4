0156333ea411fbfe3cc7d16a06a0849248ab8874
Marcus Parmentier <marcusjparmentier@gmail.com>
2017-11-09 01:27:06 -0800

    Add the controller and view for bandDetails fully funtional.

--- BandTracker/Controllers/HomeController.cs
+++ BandTracker/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace BandTracker.Controllers
         return View();
     }
 
+    //Bands
     [HttpGet("/bands")]
     public ActionResult Bands()
     {
@@ -32,6 +33,29 @@ namespace BandTracker.Controllers
     {
         Band newBand = new Band(Request.Form["band-bandName"], Request.Form["band-genre"]);
         newBand.Save();
+        return View("Success");
+    }
+
+    [HttpGet("/bands/{id}")]
+    public ActionResult BandDetails(int id)
+    {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Band SelectedBand = Band.Find(id);
+        List<Venue> BandVenues = SelectedBand.GetVenues();
+        List<Venue> AllVenues = Venue.GetAll();
+        model.Add("band", SelectedBand);
+        model.Add("bandVenues", BandVenues);
+        model.Add("allVenues", AllVenues);
+        return View(model);
+    }
+
+    [HttpPost("bands/{bandId}/venues/new")]
+    public ActionResult BandAddVenue(int bandId)
+    {
+        Band band = Band.Find(bandId);
+        Venue venue = Venue.Find(Int32.Parse(Request.Form["venue-id"]));
+
+        band.AddVenue(venue);
         return View("Success");
     }
 
